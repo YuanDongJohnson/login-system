b08aac41c73494b0cfde37cfc027415549855243
YuanDongJohnson <163552945+YuanDongJohnson@users.noreply.github.com>
2024-12-04 14:06:35 +0800

    Update route.ts

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -4,13 +4,14 @@ import { NextResponse } from 'next/server'
 export async function GET(request: Request) {
   const requestUrl = new URL(request.url)
   const code = requestUrl.searchParams.get('code')
+  const redirect = requestUrl.searchParams.get('redirect') || '/'
 
   if (code) {
     const supabase = createClient()
     await supabase.auth.exchangeCodeForSession(code)
   }
 
-  // URL to redirect to after sign in process completes
-  return NextResponse.redirect(requestUrl.origin)
+  // 重定向到指定的页面，默认为首页
+  return NextResponse.redirect(new URL(redirect, requestUrl.origin))
 }
 
