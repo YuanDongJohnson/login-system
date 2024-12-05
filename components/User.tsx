27982e2c092695c570e91e2509277aa297d71b19
YuanDongJohnson <163552945+YuanDongJohnson@users.noreply.github.com>
2024-12-05 08:36:29 +0800

    Update User.tsx

--- components/User.tsx
+++ components/User.tsx
@@ -4,38 +4,28 @@ import { redirect } from 'next/navigation';
 export default async function User() {
   const supabase = createClient();
 
-  try {
-    const {
-      data: { session },
-    } = await supabase.auth.getSession();
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
 
-    const signOut = async (event) => {
-      'use server';
+  const signOut = async () => {
+    'use server';
 
-      event.preventDefault(); // 阻止表单默认提交行为
+    const supabase = createClient();
+    await supabase.auth.signOut();
+    return redirect('/login');
+  };
 
-      const supabase = createClient();
-      await supabase.auth.signOut();
-      return redirect('/login');
-    };
-
-    return (
-      session && (
-        <div className="flex items-center gap-4">
-          你好,欢迎 {session.user.email}!
-          <form onSubmit={signOut}>
-            <button
-              className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
-              type="submit"
-            >
-              登出
-            </button>
-          </form>
-        </div>
-      )
-    );
-  } catch (error) {
-    console.error('Error getting session:', error);
-    return <div>无法获取会话信息</div>;
-  }
+  return (
+    session && (
+      <div className="flex items-center gap-4">
+        Hey, {session.user.email}!
+        <form action={signOut}>
+          <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
+            Logout
+          </button>
+        </form>
+      </div>
+    )
+  );
 }
