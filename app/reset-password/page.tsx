442887fac8a6a765742c08493ebeab9eb6283211
YuanDongJohnson <163552945+YuanDongJohnson@users.noreply.github.com>
2024-12-09 07:13:06 +0800

    Update page.tsx

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/Header/Header';
 import { createClient } from '@/utils/supabase/server';
-import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
 export default async function ResetPassword({
   searchParams,
@@ -8,63 +9,68 @@ export default async function ResetPassword({
   searchParams: { message: string; code: string };
 }) {
   const supabase = createClient();
-  const router = useRouter();
 
   const {
     data: { session },
   } = await supabase.auth.getSession();
 
   if (session) {
-    router.push('/login');
+    return redirect('/login');
   }
 
-  const resetPassword = async (event) => {
+  const resetPassword = async (formData: FormData) => {
     'use server';
-    event.preventDefault();
 
-    const formData = new FormData(event.currentTarget);
     const password = formData.get('password') as string;
+    const supabase = createClient();
 
-    const { error } = await supabase.auth.exchangeCodeForSession(
-      searchParams.code
-    );
+    if (searchParams.code) {
+      const supabase = createClient();
+      const { error } = await supabase.auth.exchangeCodeForSession(
+        searchParams.code
+      );
 
-    if (error) {
-      router.push(`/reset-password?message=无法重置密码,链接过期!`);
-      return;
+      if (error) {
+        return redirect(
+          `/reset-password?message=无法重置密码,链接过期!`
+        );
+      }
     }
 
-    const { user } = await supabase.auth.getUser(); // 获取用户信息
-    const { error: updateError } = await supabase.auth.updateUser(user.id, {
+    const { error } = await supabase.auth.updateUser({
       password,
     });
 
-    if (updateError) {
-      console.log(updateError);
-      router.push(`/reset-password?message=无法重置密码,请重试!`);
-    } else {
-      router.push(`/login?message=你的密码已重置,请重新登入`);
+    if (error) {
+      console.log(error);
+      return redirect(
+        `/reset-password?message=无法重置密码,请重试!`
+      );
     }
+
+    redirect(
+      `/login?message=你的密码已重置,请重新登入`
+    );
   };
 
   return (
     <div>
       <Header />
 
-      <button
+      <Link
+        href="/"
         className="py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover text-sm m-4"
-        onClick={() => router.push('/')}
       >
         回首页
-      </button>
+      </Link>
 
       <div className="w-full px-8 sm:max-w-md mx-auto mt-4">
         <form
           className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground mb-4"
-          onSubmit={resetPassword}
+          action={resetPassword}
         >
           <label className="text-md" htmlFor="password">
-            设置新密码
+            输入新密码
           </label>
           <input
             className="rounded-md px-4 py-2 bg-inherit border mb-6"
