afb813fe31cb27bbcf05bed6ba6344c9f22cf1e5
YuanDongJohnson <163552945+YuanDongJohnson@users.noreply.github.com>
2024-12-09 07:50:28 +0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,26 @@
-import { type NextRequest } from "next/server";
-import { updateSession } from "@/utils/supabase/middleware";
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { NextResponse } from 'next/server'
 
-export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+import type { NextRequest } from 'next/server'
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next()
+  const supabase = createMiddlewareClient({ req, res })
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  // 如果用户已登录并尝试访问登录或重置密码页面，则重定向到主页
+  if (session && (req.nextUrl.pathname === '/login' || req.nextUrl.pathname === '/reset-password')) {
+    return NextResponse.redirect(new URL('/', req.url))
+  }
+
+  // 对于其他路由，继续正常处理
+  return res
 }
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
-     * Feel free to modify this pattern to include more paths.
-     */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
-  ],
-};
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+}
+
