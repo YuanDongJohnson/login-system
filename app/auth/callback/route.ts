68ac1e02dab77a4e840af4e66d83d7d00d6e1f65
YuanDongJohnson <163552945+YuanDongJohnson@users.noreply.github.com>
2024-12-08 23:49:48 +0800

    Update route.ts

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -9,7 +9,7 @@ export async function GET(request: Request) {
 
   if (code) {
     const cookieStore = cookies()
-    const supabase = createClient(cookieStore)
+    const supabase = createClient()
     
     try {
       const { error } = await supabase.auth.exchangeCodeForSession(code)
