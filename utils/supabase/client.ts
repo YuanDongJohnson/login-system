5607c696e3f27520ff4abe3794b2874249017e23
YuanDongJohnson <163552945+YuanDongJohnson@users.noreply.github.com>
2024-12-13 09:36:03 +0800

    Update client.ts

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,7 +1,17 @@
-import { createBrowserClient } from "@supabase/ssr";
+import { createBrowserClient, createServerClient } from "@supabase/ssr";
 
-export const createClient = () =>
-  createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  );
+export function createClient() {
+  if (typeof window === 'undefined') {
+    // 在服务器端
+    return createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.SUPABASE_SERVER_KEY!, // 服务器端密钥不应该暴露给客户端
+    );
+  } else {
+    // 在客户端
+    return createBrowserClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    );
+  }
+}
