eb69c4b1c8702bcfc73eb5d5ca1ae54102b6d472
YuanDongJohnson <163552945+YuanDongJohnson@users.noreply.github.com>
2024-12-13 08:25:15 +0800

    Create actions.ts

---
+++ app/actions.ts
@@ -0,0 +1,22 @@
+// app/actions.ts
+'use server'
+
+import { createClient } from '@/utils/supabase/client';
+import { redirect } from 'next/navigation';
+
+export async function signIn(formData: FormData) {
+  const email = formData.get('email') as string;
+  const password = formData.get('password') as string;
+  const supabase = createClient();
+
+  const { error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) {
+    throw new Error('无法验证的用户');
+  }
+
+  return redirect('/text');
+}
