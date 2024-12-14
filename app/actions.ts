d1537ff34d582b063f15b5aa81d7acb2f2c52030
YuanDongJohnson <163552945+YuanDongJohnson@users.noreply.github.com>
2024-12-14 10:33:24 +0800

    Update actions.ts

--- app/actions.ts
+++ app/actions.ts
@@ -5,18 +5,18 @@ import { createClient } from '@/utils/supabase/client';
 import { redirect } from 'next/navigation';
 
 export async function signIn(formData: FormData) {
-  const email = formData.get('email') as string;
-  const password = formData.get('password') as string;
-  const supabase = createClient();
+const email = formData.get('email') as string;
+const password = formData.get('password') as string;
+const supabase = createClient();
 
-  const { error } = await supabase.auth.signInWithPassword({
-    email,
-    password,
-  });
+const { error } = await supabase.auth.signInWithPassword({
+email,
+password,
+});
 
-  if (error) {
-    throw new Error('无法验证的用户');
-  }
+if (error) {
+throw new Error('无法验证的用户');
+}
 
-  return redirect('/text');
+return redirect('/text');
 }
