59675b00de3e010c79e8d4db932fd8bb63d4fa3c
YuanDongJohnson <163552945+YuanDongJohnson@users.noreply.github.com>
2024-12-14 11:11:19 +0800

    Update client.ts

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,14 +1,7 @@
-import { createBrowserClient } from '@supabase/ssr';
+import { createBrowserClient } from "@supabase/ssr";
 
-export const createClient = () => {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-  if (!supabaseUrl || !supabaseAnonKey) {
-    console.error('Missing Supabase environment variables');
-    return null;
-  }
-
-  return createBrowserClient(supabaseUrl, supabaseAnonKey);
-};
-
+export const createClient = () =>
+  createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  );
