2df1d028f569a1edbe2febc2ab56252d6cf29656
YuanDongJohnson <163552945+YuanDongJohnson@users.noreply.github.com>
2024-12-15 22:38:37 +0800

    Update PasswordLoginForm.tsx

--- components/PasswordLoginForm.tsx
+++ components/PasswordLoginForm.tsx
@@ -1,51 +1,49 @@
 'use client';
-import { useState } from 'react';
-import Link from 'next/link';
-import { createClient } from '@/utils/supabase/client'; // 确保路径正确
 
+import Link from 'next/link';
 
 interface PasswordLoginFormProps {
-searchParams: { message: string };
-signInAction: (formData: FormData) => Promise<void>;
+  searchParams: { message: string };
+  signInAction: (formData: FormData) => Promise<void>;
 }
 
 export function PasswordLoginForm({ searchParams, signInAction }: PasswordLoginFormProps) {
-return (
-<form
-className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
-onSubmit={(event) => {
-event.preventDefault();
-const formData = new FormData(event.currentTarget);
-signInAction(formData);
-}}
->
-<input
-className="rounded-md px-4 py-2 bg-inherit border mb-4"
-name="email"
-placeholder="邮箱"
-required
-/>
-<input
-className="rounded-md px-4 py-2 bg-inherit border mb-4"
-type="password"
-name="password"
-placeholder="密码"
-required
-/>
-<button className="bg-indigo-700 rounded-md px-4 py-2 text-foreground mb-2">
-登录/注册
-</button>
+  return (
+    <form
+      className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
+      onSubmit={(event) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        signInAction(formData);
+      }}
+    >
+      <input
+        className="rounded-md px-4 py-2 bg-inherit border mb-4"
+        name="email"
+        placeholder="邮箱"
+        required
+      />
+      <input
+        className="rounded-md px-4 py-2 bg-inherit border mb-4"
+        type="password"
+        name="password"
+        placeholder="密码"
+        required
+      />
+      <button className="bg-indigo-700 rounded-md px-4 py-2 text-foreground mb-2">
+        登录/注册
+      </button>
 
-{searchParams?.message && (
-<p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
-{searchParams.message}
-</p>
-)}
+      {searchParams?.message && (
+        <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
+          {searchParams.message}
+        </p>
+      )}
 
-<Link href="/forgot-password" className="text-sm text-indigo-400 text-center mt-2">
-  <a>忘记密码</a>
-</Link>
-
-</form>
-);
+      <Link href="/forgot-password" className="text-sm text-indigo-400 text-center mt-2">
+        忘记密码
+      </Link>
+    </form>
+  );
 }
+
