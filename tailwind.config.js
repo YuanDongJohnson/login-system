5971eb79df7e0c661818367f6221901df7242d18
YuanDongJohnson <163552945+YuanDongJohnson@users.noreply.github.com>
2024-12-15 08:10:30 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,20 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}', // Next.js 页面
-    './components/**/*.{js,ts,jsx,tsx}', // 组件
-    './app/**/*.{js,ts,jsx,tsx}', // Next.js app 目录
-    './layout/**/*.{js,ts,jsx,tsx}', // Next.js layout 目录（如果有）
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        btn: {
-          background: "hsl(var(--btn-background))",
-          'background-hover': "hsl(var(--btn-background-hover))",
-        },
-      },
-    },
-  },
-  plugins: [],
+content: [
+"./app/**/*.{js,ts,jsx,tsx,mdx}",
+"./components/**/*.{js,ts,jsx,tsx,mdx}",
+],
+theme: {
+extend: {
+colors: {
+background: "hsl(var(--background))",
+foreground: "hsl(var(--foreground))",
+btn: {
+background: "hsl(var(--btn-background))",
+"background-hover": "hsl(var(--btn-background-hover))",
+},
+},
+},
+},
+plugins: [],
 };
