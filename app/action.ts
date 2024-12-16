786bb6061ba8c7edf01aa65ee06d6a46af983a17
YuanDongJohnson <163552945+YuanDongJohnson@users.noreply.github.com>
2024-12-16 08:37:44 +0800

    Create action.ts

---
+++ app/action.ts
@@ -0,0 +1,31 @@
+'use server'
+
+import { createClient } from '@/utils/supabase/server'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+export async function signIn(formData: FormData) {
+  const email = formData.get('email') as string
+  const password = formData.get('password') as string
+  const cookieStore = cookies()
+  const supabase = createClient(cookieStore)
+
+  const { error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  })
+
+  if (error) {
+    return { error: error.message }
+  }
+
+  return { error: null }
+}
+
+export async function signOut() {
+  const cookieStore = cookies()
+  const supabase = createClient(cookieStore)
+  await supabase.auth.signOut()
+  return redirect('/login')
+}
+
