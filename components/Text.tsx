f49b958d5cd55a395f636a5fc3220f875f8b649d
YuanDongJohnson <163552945+YuanDongJohnson@users.noreply.github.com>
2024-12-17 21:40:06 +0800

    Update Text.tsx

--- components/Text.tsx
+++ components/Text.tsx
@@ -1,4 +1,3 @@
-import User from '@/components/User';
 export const Text = () => {
   return (
     <div className="relative isolate px-6 lg:px-8">
@@ -33,10 +32,6 @@ export const Text = () => {
             clipPath: 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
           }}
         ></div>
-      </div>
-      {/* User组件布局在右上方 */}
-      <div className="absolute top-0 right-0">
-        <User />
       </div>
     </div>
   );
