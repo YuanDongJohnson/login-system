9588a152c20b4882d665810f7f8407344564d8ff
YuanDongJohnson <163552945+YuanDongJohnson@users.noreply.github.com>
2024-12-17 13:31:31 +0800

    Update Text.tsx

--- components/Text.tsx
+++ components/Text.tsx
@@ -1,5 +1,3 @@
-import User from '../User';
-
 export const Text = () => {
   return (
     <div className="relative isolate px-6 lg:px-8">
@@ -13,9 +11,6 @@ export const Text = () => {
             clipPath: 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
           }}
         ></div>
-      </div>
-      <div className="absolute top-4 right-4 z-20">
-        <User />
       </div>
       <div className="mx-auto max-w-2xl lg:py-16">
         <div className="text-center">
@@ -41,4 +36,3 @@ export const Text = () => {
     </div>
   );
 };
-
