a88d66a380c0c95813768a6b7c3c919d5b05823a
YuanDongJohnson <163552945+YuanDongJohnson@users.noreply.github.com>
2024-12-17 12:54:58 +0800

    Update page.tsx

--- app/text/page.tsx
+++ app/text/page.tsx
@@ -15,18 +15,16 @@ export default async function TextPage() {
   return (
     <div className="relative min-h-screen">
       <div className="fixed top-0 left-0 right-0 z-10">
-        <div className="relative">
-          <Header className="w-full" />
-          <div className="absolute top-0 right-0 z-20 p-2">
-            <User />
-          </div>
-        </div>
+        <Header className="w-full" />
       </div>
-      <div className="pt-16 px-4">
+      <div className="fixed top-2 right-2 z-20">
+        <User />
+      </div>
+      <main className="pt-16 px-4">
         <div className="flex flex-col items-center">
           <Text />
         </div>
-      </div>
+      </main>
     </div>
   );
 }
