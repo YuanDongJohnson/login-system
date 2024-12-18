2f59848b8a4b31ff4a4a5fdd57be76ff35355327
YuanDongJohnson <163552945+YuanDongJohnson@users.noreply.github.com>
2024-12-18 09:14:29 +0800

    Update Header.tsx

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,11 +1,11 @@
-
+import User from '../User';
 import classes from './Header.module.css';
 
 export default async function Header() {
   return (
     <div className={classes.header}>
       <h3>老吴手工艺展馆</h3>
-     
+      <User />
     </div>
   );
 }
