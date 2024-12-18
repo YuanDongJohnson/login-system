429e0477d54e699093aac7299dfebb229a9aa5eb
YuanDongJohnson <163552945+YuanDongJohnson@users.noreply.github.com>
2024-12-18 09:19:25 +0800

    Update User.tsx

--- components/User.tsx
+++ components/User.tsx
@@ -1,18 +1,18 @@
 'use client'
 
-import { createClient } from '@/utils/supabase/client'
+import { createClient } from '@/utils/supabase/client'; // 确保路径正确
 import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
 
 export default function User() {
-  const [email, setEmail] = useState<string | null>(null)
+  const [userIdentifier, setUserIdentifier] = useState<string | null>(null)
   const supabase = createClient()
   const router = useRouter()
 
   useEffect(() => {
     const getUser = async () => {
       const { data: { user } } = await supabase.auth.getUser()
-      setEmail(user?.email ?? null)
+      setUserIdentifier(user?.phone || user?.email || null)
     }
     getUser()
   }, [supabase.auth])
@@ -23,9 +23,9 @@ export default function User() {
   }
 
   return (
-    email && (
+    userIdentifier && (
       <div className="flex items-center gap-4">
-        亲爱的, {email} 你好!
+       {userIdentifier} 你好!
         <button 
           onClick={signOut}
           className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
