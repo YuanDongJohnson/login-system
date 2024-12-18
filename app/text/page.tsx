741128ba89dd82716a111d21f496d52ba7296f95
YuanDongJohnson <163552945+YuanDongJohnson@users.noreply.github.com>
2024-12-18 09:13:24 +0800

    Update page.tsx

--- app/text/page.tsx
+++ app/text/page.tsx
@@ -1,33 +1,27 @@
 import Header from '@/components/Header/Header';
-import User from '@/components/User';
 import { Text } from '@/components/Text';
 import { createClient } from '@/utils/supabase/server';
 import { redirect } from 'next/navigation';
 
+
 export default async function TextPage() {
-  // 创建 Supabase 客户端实例
-  const supabase = createClient();
+// 创建 Supabase 客户端实例
+const supabase = createClient();
 
-  // 获取当前会话信息
-  const { data: { session } } = await supabase.auth.getSession();
+// 获取当前会话信息
+const { data: { session } } = await supabase.auth.getSession();
 
-  // 检查用户是否已登录
-  if (!session) {
-    // 如果未登录，重定向到首页
-    return redirect('/login');
-  }
+// 检查用户是否已登录
+if (!session) {
+// 如果未登录，重定向到首页
+return redirect('/login');
+}
 
-  // 用户已登录，渲染页面内容
-  return (
-    <div className="relative"> {/* 添加了一个相对定位的容器 */}
-      <div className="header"> {/* 应用.header样式 */}
-        <Header />
-      </div>
-      <Text />
-      {/* User组件也应用.header样式，并放置在Header旁边 */}
-      <div className="header">
-        <User />
-      </div>
-    </div>
-  );
+// 用户已登录，渲染页面内容
+return (
+<div>
+<Header />
+<Text />
+</div>
+);
 }
