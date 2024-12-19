cbd4a5d8b6f2c9345db0e351ccf6bac66bfffe02
YuanDongJohnson <163552945+YuanDongJohnson@users.noreply.github.com>
2024-12-19 08:23:42 +0800

    Update page.tsx

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -14,42 +14,54 @@ export default async function ResetPassword({
     data: { session },
   } = await supabase.auth.getSession();
 
+  // 如果用户已经登录，直接重定向到登录页面
   if (session) {
-    return redirect('/');
+    return redirect('/login');
+  }
+
+  // 检查是否通过正确的方式访问重置密码页面
+  // 直接输入网址的用户没有 searchParams.code，因此会被重定向到登录页面
+  if (!searchParams.code) {
+    return redirect('/login');
   }
 
   const resetPassword = async (formData: FormData) => {
     'use server';
 
     const password = formData.get('password') as string;
-    const supabase = createClient();
+    const confirm_password = formData.get('confirmPassword') as string;
 
-    if (searchParams.code) {
-      const supabase = createClient();
-      const { error } = await supabase.auth.exchangeCodeForSession(
-        searchParams.code
+    // 确认密码是否匹配
+    if (password !== confirm_password) {
+      return redirect(
+        `/reset-password?message=两次输入的密码不一致，请重新输入`
       );
-
-      if (error) {
-        return redirect(
-          `/reset-password?message=Unable to reset Password. Link expired!`
-        );
-      }
     }
 
-    const { error } = await supabase.auth.updateUser({
+    const supabase = createClient();
+    const { error } = await supabase.auth.exchangeCodeForSession(
+      searchParams.code
+    );
+
+    if (error) {
+      return redirect(
+        `/reset-password?message=链接过期,无法重置密码`
+      );
+    }
+
+    const { error: updateError } = await supabase.auth.updateUser({
       password,
     });
 
-    if (error) {
-      console.log(error);
+    if (updateError) {
+      console.log(updateError);
       return redirect(
-        `/reset-password?message=Unable to reset Password. Try again!`
+        `/reset-password?message=无法重置密码,再试一次`
       );
     }
 
     redirect(
-      `/login?message=Your Password has been reset successfully. Sign in.`
+      `/login?message=你的密码已重置,请登入`
     );
   };
 
@@ -61,44 +73,50 @@ export default async function ResetPassword({
         href="/"
         className="py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover text-sm m-4"
       >
-        Home
+        回首页
       </Link>
 
       <div className="w-full px-8 sm:max-w-md mx-auto mt-4">
-        <form
-          className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground mb-4"
-          action={resetPassword}
-        >
-          <label className="text-md" htmlFor="password">
-            New Password
-          </label>
-          <input
-            className="rounded-md px-4 py-2 bg-inherit border mb-6"
-            type="password"
-            name="password"
-            placeholder="••••••••"
-            required
-          />
-          <label className="text-md" htmlFor="password">
-            Confirm New Password
-          </label>
-          <input
-            className="rounded-md px-4 py-2 bg-inherit border mb-6"
-            type="password"
-            name="confirmPassword"
-            placeholder="••••••••"
-            required
-          />
-          <button className="bg-indigo-700 rounded-md px-4 py-2 text-foreground mb-2">
-            Reset
-          </button>
+        {searchParams.code ? (
+          <form
+            className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground mb-4"
+            action={resetPassword}
+          >
+            <label className="text-md" htmlFor="password">
+              输入新密码
+            </label>
+            <input
+              className="rounded-md px-4 py-2 bg-inherit border mb-6"
+              type="password"
+              name="password"
+              placeholder="••••••••"
+              required
+            />
+            <label className="text-md" htmlFor="password">
+              确认新密码
+            </label>
+            <input
+              className="rounded-md px-4 py-2 bg-inherit border mb-6"
+              type="password"
+              name="confirmPassword"
+              placeholder="••••••••"
+              required
+            />
+            <button className="bg-indigo-700 rounded-md px-4 py-2 text-foreground mb-2">
+              重置密码
+            </button>
 
-          {searchParams?.message && (
-            <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
-              {searchParams.message}
-            </p>
-          )}
-        </form>
+            {searchParams?.message && (
+              <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
+                {searchParams.message}
+              </p>
+            )}
+          </form>
+        ) : (
+          <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
+            您没有权限访问此页面。请通过电子邮件提供的链接访问。
+          </p>
+        )}
       </div>
     </div>
   );
