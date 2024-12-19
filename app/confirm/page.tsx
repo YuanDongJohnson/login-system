781e4318d5d6547907eed8a1cf0edb923c07e8e6
YuanDongJohnson <163552945+YuanDongJohnson@users.noreply.github.com>
2024-12-19 10:50:59 +0800

    Update page.tsx

--- app/confirm/page.tsx
+++ app/confirm/page.tsx
@@ -1,16 +1,27 @@
 import Header from '@/components/Header/Header';
+import { useEffect } from 'react';
 
 export default function Signup({
   searchParams,
 }: {
   searchParams: { message: string };
 }) {
+  useEffect(() => {
+    // 设置3秒后跳转到登录页面
+    const timer = setTimeout(() => {
+      window.location.href = '/login';
+    }, 3000);
+
+    // 清除定时器，防止组件卸载后执行跳转
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div>
       <Header />
 
       <div className="w-full px-8 sm:max-w-lg mx-auto mt-8">
-        <p className="text-foreground">{searchParams.message}</p>
+        <p className="text-foreground">注册成功！{searchParams.message}</p>
       </div>
     </div>
   );
