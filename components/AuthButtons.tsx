8c51de006dc9809b1ec7b02b07c90e3c25a27e62
YuanDongJohnson <163552945+YuanDongJohnson@users.noreply.github.com>
2024-12-24 14:28:54 +0800

    Update AuthButtons.tsx

--- components/AuthButtons.tsx
+++ components/AuthButtons.tsx
@@ -19,13 +19,13 @@ export const AuthButtons = async () => {
         href="/login"
         className="rounded-md border border-indigo-500 bg-indigo-600 px-6 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
       >
-        Login
+        登入
       </Link>
       <Link
         href="/signup"
         className="text-sm font-semibold leading-6 text-gray-200 rounded-md border border-indigo-500 py-2 px-6 hover:border-indigo-300"
       >
-        Signup
+        注册
       </Link>
     </div>
   );
