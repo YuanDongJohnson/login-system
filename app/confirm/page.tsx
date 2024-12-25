d2b5608046922de25b086bf1d3696a081d737d1b
YuanDongJohnson <163552945+YuanDongJohnson@users.noreply.github.com>
2024-12-26 06:54:33 +0800

    Update page.tsx

--- app/confirm/page.tsx
+++ app/confirm/page.tsx
@@ -1,36 +1,17 @@
-
 import Header from '@/components/Header/Header';
 
-import './globals.css'; // 确保引入了全局CSS文件
-
-
-
 export default function Signup({
-
   searchParams,
-
 }: {
-
   searchParams: { message: string };
-
 }) {
-
   return (
-
-    <div className="font-geist">
-
+    <div>
       <Header />
 
-
-
       <div className="w-full px-8 sm:max-w-lg mx-auto mt-8">
-
         <p className="text-foreground">{searchParams.message}</p>
-
       </div>
-
     </div>
-
   );
-
 }
