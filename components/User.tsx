140cb3b23902e388d19a57c61b7b6985d00d02a8
YuanDongJohnson <163552945+YuanDongJohnson@users.noreply.github.com>
2024-12-26 07:28:08 +0800

    Add files via upload

--- components/User.tsx
+++ components/User.tsx
@@ -1,31 +1,38 @@
-import { createClient } from '@/utils/supabase/server';
-import { redirect } from 'next/navigation';
+'use client'
 
-export default async function User() {
-  const supabase = createClient();
+import { createClient } from '@/utils/supabase/client'; // 确保路径正确
+import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
+export default function User() {
+  const [userIdentifier, setUserIdentifier] = useState<string | null>(null)
+  const supabase = createClient()
+  const router = useRouter()
+
+  useEffect(() => {
+    const getUser = async () => {
+      const { data: { user } } = await supabase.auth.getUser()
+      setUserIdentifier(user?.phone || user?.email || null)
+    }
+    getUser()
+  }, [supabase.auth])
 
   const signOut = async () => {
-    'use server';
-
-    const supabase = createClient();
-    await supabase.auth.signOut();
-    return redirect('/login');
-  };
+    await supabase.auth.signOut()
+    router.push('/login')
+  }
 
   return (
-    session && (
+    userIdentifier && (
       <div className="flex items-center gap-4">
-        你好, {session.user.email}!
-        <form action={signOut}>
-          <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
-            登出
-          </button>
-        </form>
+       {userIdentifier} 你好!
+        <button 
+          onClick={signOut}
+          className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
+        >
+          登出
+        </button>
       </div>
     )
-  );
+  )
 }
