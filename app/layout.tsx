d4e4584e7f10d106b078e1871febbabedbfd9d1f
YuanDongJohnson <163552945+YuanDongJohnson@users.noreply.github.com>
2024-12-25 09:54:08 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,42 @@
+
 import { GeistSans } from 'geist/font/sans';
+
 import './globals.css';
 
+
+
 export const metadata = {
+
   title: '欢迎来到老吴手工艺品展馆',
+
   description: '',
+
 };
 
+
+
 export default function RootLayout({
+
   children,
+
 }: {
+
   children: React.ReactNode;
+
 }) {
+
   return (
-    <html lang="en" className={GeistSans.className}>
+
+    <html lang="zh" className={GeistSans.className}>
+
       <body className="bg-background text-foreground">
+
         <main className="min-h-screen">{children}</main>
+
       </body>
+
     </html>
+
   );
+
 }
