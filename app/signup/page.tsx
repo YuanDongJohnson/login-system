5a1e7433f4f6e3ad528459a65acd6f304651182b
YuanDongJohnson <163552945+YuanDongJohnson@users.noreply.github.com>
2024-12-30 07:17:13 +0800

    Update page.tsx

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,158 +1,114 @@
-
-'use client'
-
-
-
-import { useState } from 'react';
-
-import { useRouter } from 'next/navigation';
-
-import { createClient } from '@/utils/supabase/client';
-
+import Header from '@/components/Header/Header';
+import { createClient } from '@/utils/supabase/server';
 import Link from 'next/link';
-
+import { redirect } from 'next/navigation';
+import { headers } from 'next/headers';
 import Toast from '@/components/Toast';
 
-
-
-export default function Signup() {
-
-  const [message, setMessage] = useState('');
-
-  const [showToast, setShowToast] = useState(false);
-
-  const router = useRouter();
-
+export default async function Signup({
+  searchParams,
+}: {
+  searchParams: { message: string };
+}) {
   const supabase = createClient();
 
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
 
+  if (session) {
+    return redirect('/text');
+  }
 
   const signUp = async (formData: FormData) => {
+    'use server';
 
+    const origin = headers().get('origin');
     const email = formData.get('email') as string;
-
     const password = formData.get('password') as string;
-
     const confirmPassword = formData.get('confirmPassword') as string;
-
-
+    const supabase = createClient();
 
     if (password !== confirmPassword) {
-
-      setMessage('密码不匹配');
-
-      setShowToast(true);
-
-      return;
-
+      return redirect('/signup?message=' + encodeURIComponent('密码不匹配'));
     }
 
-
-
-    const { data, error } = await supabase.auth.signUp({
-
+    const { error } = await supabase.auth.signUp({
       email,
-
       password,
-
       options: {
-
-        emailRedirectTo: `
-{window.location.origin}/auth/callback`,
-
+        emailRedirectTo: `${origin}/auth/callback`,
       },
-
     });
 
-
-
     if (error) {
-
-      setMessage('无法注册用户');
-
-      setShowToast(true);
-
-    } else if (data) {
-
-      const confirmMessage = encodeURIComponent(`请查看邮箱 (
-{email}) 以完成注册流程`);
-
-      router.push(`/confirm?message=${confirmMessage}`);
-
+      return redirect('/signup?message=' + encodeURIComponent('无法注册用户'));
     }
 
+    return redirect('/signup?message=' + encodeURIComponent(`请查看邮箱 (${email}) 以完成注册流程`));
   };
 
-
-
   return (
-
     <div>
-
       <Header />
 
-
-
       <Link
-
         href="/"
-
         className="py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover text-sm m-4"
-
       >
-
         回首页
-
       </Link>
 
-
-
       <div className="w-full px-8 sm:max-w-md mx-auto mt-4">
-
         <form
-
           className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground mb-4"
-
-          onSubmit={(e) => {
-
-            e.preventDefault();
-
-            signUp(new FormData(e.currentTarget));
-
-          }}
-
+          action={signUp}
         >
-
-          {/* 表单内容 */}
-
+          <label className="text-md" htmlFor="email">
+            输入电子邮箱
+          </label>
+          <input
+            className="rounded-md px-4 py-2 bg-inherit border mb-6"
+            name="email"
+            placeholder="you@example.com"
+            required
+          />
+          <label className="text-md" htmlFor="password">
+            输入密码
+          </label>
+          <input
+            className="rounded-md px-4 py-2 bg-inherit border mb-6"
+            type="password"
+            name="password"
+            placeholder="••••••••"
+            required
+          />
+          <label className="text-md" htmlFor="confirmPassword">
+            确认密码
+          </label>
+          <input
+            className="rounded-md px-4 py-2 bg-inherit border mb-6"
+            type="password"
+            name="confirmPassword"
+            placeholder="••••••••"
+            required
+          />
+          <button type="submit" className="bg-indigo-700 rounded-md px-4 py-2 text-foreground mb-2">
+            注册
+          </button>
         </form>
 
-
-
         <Link
-
           href="/login"
-
           className="rounded-md no-underline text-foreground text-sm"
-
         >
-
           已经有帐号？去登录
-
         </Link>
-
       </div>
 
-
-
-      {showToast && (
-
-        <Toast message={message} onClose={() => setShowToast(false)} isVisible={showToast} />
-
+      {searchParams?.message && (
+        <Toast message={decodeURIComponent(searchParams.message)} />
       )}
-
     </div>
-
   );
-
 }
