a6760a6fb8191aaf8bb9522e39efbfae9017f0ab
YuanDongJohnson <163552945+YuanDongJohnson@users.noreply.github.com>
2024-12-31 11:02:02 +0800

    Update page.tsx

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,68 +1,101 @@
-import Header from '@/components/Header/Header';
-import { createClient } from '@/utils/supabase/server';
-import Link from 'next/link';
-import { redirect } from 'next/navigation';
-import { headers } from 'next/headers';
-import { SignupForm } from '@/components/SignupForm';
+'use client';
 
-export default async function Signup({
-  searchParams,
-}: {
-  searchParams: { message: string };
-}) {
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import Header from '@/components/Header/Header';
+import { createClient } from '@/utils/supabase/client';
+import Link from 'next/link';
+import Toast from '@/components/Toast';
+
+export default function Signup() {
+  const [toastMessage, setToastMessage] = useState<string | null>(null);
+  const router = useRouter();
   const supabase = createClient();
 
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
-
-  if (session) {
-    return redirect('/text');
-  }
-
   const signUp = async (formData: FormData) => {
-    'use server';
-
-    const origin = headers().get('origin');
+    setToastMessage(null);
     const email = formData.get('email') as string;
     const password = formData.get('password') as string;
-    const supabase = createClient();
+    const confirmPassword = formData.get('confirmPassword') as string;
+
+    if (password !== confirmPassword) {
+      setToastMessage('密码不匹配');
+      return;
+    }
 
     const { error } = await supabase.auth.signUp({
       email,
       password,
       options: {
-        emailRedirectTo: `${origin}/auth/callback`,
+        emailRedirectTo: `${window.location.origin}/auth/callback`,
       },
     });
 
     if (error) {
-      return redirect('/signup?message=' + encodeURIComponent('无法注册用户'));
+      setToastMessage('无法注册用户');
+    } else {
+      setToastMessage(`请查看邮箱 (${email}) 以完成注册流程`);
     }
-
-    return redirect('/signup?message=' + encodeURIComponent(`请查看邮箱 (${email}) 以完成注册流程`));
   };
 
   return (
     <div>
       <Header />
-
       <Link
         href="/"
         className="py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover text-sm m-4"
       >
         回首页
       </Link>
-
       <div className="w-full px-8 sm:max-w-md mx-auto mt-4">
-        <SignupForm signUp={signUp} />
+        <form
+          className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground mb-4"
+          onSubmit={(e) => {
+            e.preventDefault();
+            signUp(new FormData(e.currentTarget));
+          }}
+        >
+          <label className="text-md" htmlFor="email">
+            输入电子邮箱
+          </label>
+          <input
+            className="rounded-md px-4 py-2 bg-inherit border mb-6"
+            name="email"
+            placeholder="you@example.com"
+            required
+          />
+          <label className="text-md" htmlFor="password">
+            输入密码
+          </label>
+          <input
+            className="rounded-md px-4 py-2 bg-inherit border mb-6"
+            type="password"
+            name="password"
+            placeholder="••••••••"
+            required
+          />
+          <label className="text-md" htmlFor="confirmPassword">
+            确认密码
+          </label>
+          <input
+            className="rounded-md px-4 py-2 bg-inherit border mb-6"
+            type="password"
+            name="confirmPassword"
+            placeholder="••••••••"
+            required
+          />
+          <button type="submit" className="bg-indigo-700 rounded-md px-4 py-2 text-foreground mb-2">
+            注册
+          </button>
+        </form>
+        <Link
+          href="/login"
+          className="rounded-md no-underline text-foreground text-sm"
+        >
+          已经有帐号？去登录
+        </Link>
       </div>
-
-      {searchParams?.message && (
-        <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
-          {searchParams.message}
-        </p>
-      )}
+      {toastMessage && <Toast message={toastMessage} />}
     </div>
   );
 }
