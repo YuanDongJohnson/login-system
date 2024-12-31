14aa336b9ef5b4660aacb58b7f01aeb3ce586de0
YuanDongJohnson <163552945+YuanDongJohnson@users.noreply.github.com>
2024-12-31 11:09:33 +0800

    Update page.tsx

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,101 +1,58 @@
-'use client';
-
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
 import Header from '@/components/Header/Header';
-import { createClient } from '@/utils/supabase/client';
+import { createClient } from '@/utils/supabase/server';
 import Link from 'next/link';
-import Toast from '@/components/Toast';
+import { redirect } from 'next/navigation';
+import { headers } from 'next/headers';
+import { SignupForm } from '@/components/SignupForm';
 
-export default function Signup() {
-  const [toastMessage, setToastMessage] = useState<string | null>(null);
-  const router = useRouter();
+export default async function Signup() {
   const supabase = createClient();
 
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (session) {
+    return redirect('/text');
+  }
+
   const signUp = async (formData: FormData) => {
-    setToastMessage(null);
+    'use server';
+
+    const origin = headers().get('origin');
     const email = formData.get('email') as string;
     const password = formData.get('password') as string;
-    const confirmPassword = formData.get('confirmPassword') as string;
-
-    if (password !== confirmPassword) {
-      setToastMessage('密码不匹配');
-      return;
-    }
+    const supabase = createClient();
 
     const { error } = await supabase.auth.signUp({
       email,
       password,
       options: {
-        emailRedirectTo: `${window.location.origin}/auth/callback`,
+        emailRedirectTo: `${origin}/auth/callback`,
       },
     });
 
     if (error) {
-      setToastMessage('无法注册用户');
-    } else {
-      setToastMessage(`请查看邮箱 (${email}) 以完成注册流程`);
+      return { error: '无法注册用户' };
     }
+
+    return { error: null };
   };
 
   return (
     <div>
       <Header />
+
       <Link
         href="/"
         className="py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover text-sm m-4"
       >
         回首页
       </Link>
+
       <div className="w-full px-8 sm:max-w-md mx-auto mt-4">
-        <form
-          className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground mb-4"
-          onSubmit={(e) => {
-            e.preventDefault();
-            signUp(new FormData(e.currentTarget));
-          }}
-        >
-          <label className="text-md" htmlFor="email">
-            输入电子邮箱
-          </label>
-          <input
-            className="rounded-md px-4 py-2 bg-inherit border mb-6"
-            name="email"
-            placeholder="you@example.com"
-            required
-          />
-          <label className="text-md" htmlFor="password">
-            输入密码
-          </label>
-          <input
-            className="rounded-md px-4 py-2 bg-inherit border mb-6"
-            type="password"
-            name="password"
-            placeholder="••••••••"
-            required
-          />
-          <label className="text-md" htmlFor="confirmPassword">
-            确认密码
-          </label>
-          <input
-            className="rounded-md px-4 py-2 bg-inherit border mb-6"
-            type="password"
-            name="confirmPassword"
-            placeholder="••••••••"
-            required
-          />
-          <button type="submit" className="bg-indigo-700 rounded-md px-4 py-2 text-foreground mb-2">
-            注册
-          </button>
-        </form>
-        <Link
-          href="/login"
-          className="rounded-md no-underline text-foreground text-sm"
-        >
-          已经有帐号？去登录
-        </Link>
+        <SignupForm signUp={signUp} />
       </div>
-      {toastMessage && <Toast message={toastMessage} />}
     </div>
   );
 }
