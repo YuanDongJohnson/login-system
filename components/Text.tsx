b0f6f484c6697060670a51c83f1032638400b24d
YuanDongJohnson <163552945+YuanDongJohnson@users.noreply.github.com>
2025-01-04 10:48:27 +0800

    Update Text.tsx

--- components/Text.tsx
+++ components/Text.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useEffect, useState } from 'react';
 import { init } from '@waline/client';
 import '@waline/client/dist/waline.css';
@@ -59,7 +60,7 @@ export const Text = () => {
   }, []);
 
   return (
-    <div className="relative isolate px-6 lg:px-8">
+    <div className="relative isolate px-6 lg:px-8 min-h-screen bg-[hsl(var(--background))] text-[hsl(var(--foreground))]">
       <div className="header bg-[#fff0f7] py-5">
         <h1 className="text-2xl font-bold">老吳動物模型手工坊</h1>
         <h2 className="text-xl">正宗純手工制造</h2>
@@ -118,29 +119,6 @@ export const Text = () => {
             5001120
           </a>
         </p>
-      </div>
-
-      <div
-        className="absolute inset-x-0 -z-10 transform-gpu overflow-hidden blur-3xl"
-        aria-hidden="true"
-      >
-        <div
-          className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-          style={{
-            clipPath: 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-          }}
-        ></div>
-      </div>
-      <div
-        className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-10rem)]"
-        aria-hidden="true"
-      >
-        <div
-          className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-          style={{
-            clipPath: 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-          }}
-        ></div>
       </div>
     </div>
   );
