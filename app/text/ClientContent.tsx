bcd4a7445e92b0b44a4b293d67fc700d05255d58
YuanDongJohnson <163552945+YuanDongJohnson@users.noreply.github.com>
2025-01-07 08:40:36 +0800

    Update ClientContent.tsx

--- app/text/ClientContent.tsx
+++ app/text/ClientContent.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function ClientContent() {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
